ddb2df4c9760f478c49128414a4433e700010721
Hvaxquez <hugoferney54@gmail.com>
2025-03-04 11:10:01 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -26,17 +26,32 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member_by_id(member_id):
+    member = jackson_family.get_member(member_id)
+    if member != None:
+        return jsonify(member), 200
+    return jsonify({"msg": "Not found"}), 404
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    request_body = request.get_json()
+    new_member = jackson_family.add_member(request_body)
+    return jsonify(new_member), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member_by_id(member_id):
+    member = jackson_family.delete_member(member_id)
+    if member != None:
+        return jsonify({ "done": True }), 200
+    return jsonify({"msg": "Not found"}), 404
 
 
-    return jsonify(response_body), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
